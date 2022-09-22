86b1342ce727c6506b2cbe983c2d7c8f1d8917bb
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2022-09-22 17:51:26 +0200

    Change - doi branch - Cleaned up comments and unused imports.

--- semaf-cli-import.py
+++ semaf-cli-import.py
@@ -1,25 +1,10 @@
-from xml.dom import minidom
 import os
 import tempfile
-#from CLARIAH_CMDI.xml2dict.processor import CMDI # load, xmldom2dict
 import json
-
-from requests.structures import CaseInsensitiveDict
-
-from SchemaLOD import Schema, GraphBuilder
-from SemafCLI import SemafUtils
-from config import default_crosswalks_location, crosswalks_location, cbs_default_crosswalks
-from rdflib import Graph, URIRef, Literal, BNode, plugin, Namespace
-from rdflib.serializer import Serializer
-from rdflib.namespace import RDF, RDFS
-#from config import cmdifile, ROOT, DATAVERSE_ID, API_TOKEN, schemaURL, cv_server, cwfile
-from config import *
-from Semaf import Semaf
-from jGraph import jGraph
 import sys
 import re
-import requests
-from datetime import datetime
+from SemafCLI import SemafUtils
+from config import *
 from pathlib import Path
 
 
@@ -76,8 +61,7 @@ if len(sys.argv) > 1:
 else:
     print("XML file required as input parameter")
     exit()
-    #cmdifile = '0b01e4108004e49d_INV_REG_REPARATIE_CONSUMENTENARTIKELEN_HANDEL_2008-01-01.dsc'
-
+    # cmdifile = '0b01e4108004e49d_INV_REG_REPARATIE_CONSUMENTENARTIKELEN_HANDEL_2008-01-01.dsc'
 
 # Load citation block
 Path(outputdir + "/citation").mkdir(parents=False, exist_ok=True)
@@ -90,12 +74,12 @@ semafcli.transformation(cmdifile, UPLOAD=UPLOAD)
 metadata = semafcli.cmdigraph.dataset
 
 # Load all other blocks
-for schema_name in schemes:    
+for schema_name in schemes:
     Path(outputdir + "/" + schema_name).mkdir(parents=False, exist_ok=True)
     custom_semafcli = SemafUtils(cbs_default_crosswalks, crosswalks_location)
-    custom_semafcli.set_nt_filename(outputdir + "/" + schema_name +"/" + "%s.nt" % output_file_name)
-    custom_semafcli.set_semaf_filename(outputdir + "/" + schema_name +"/" + "%s.json" % output_file_name)
-    custom_semafcli.set_semaf_filename_orig(outputdir + "/" + schema_name +"/" + "%s-orig.json" % output_file_name)
+    custom_semafcli.set_nt_filename(outputdir + "/" + schema_name + "/" + "%s.nt" % output_file_name)
+    custom_semafcli.set_semaf_filename(outputdir + "/" + schema_name + "/" + "%s.json" % output_file_name)
+    custom_semafcli.set_semaf_filename_orig(outputdir + "/" + schema_name + "/" + "%s-orig.json" % output_file_name)
     custom_semafcli.set_schema(schema_name=schema_name, schema_URL=schemes[schema_name])
     custom_semafcli.transformation(cmdifile, UPLOAD=UPLOAD)
     if custom_semafcli.cmdigraph.dataset:
@@ -122,21 +106,22 @@ metadatafile = outputdir + output_file_name + "-output.json"
 with open(metadatafile, 'w', encoding='utf-8') as f:
     json.dump(metadata, f, ensure_ascii=False, indent=4)
 if DATAVERSE_ID != 'liss_dc' or is_lower_level_liss_study(metadata)[0] is False:
-    #if DATAVERSE_ID == 'cbs':
+    # if DATAVERSE_ID == 'cbs':
     #    json_string = json.dumps(metadata)
     #    url = "http://0.0.0.0:8566/submit-to-datacite"
 
     #    headers = CaseInsensitiveDict()
-#         headers = {
-#             'Content-Type': 'application/json',
-#             'Authorization': 'Bearer @km1-10122004-lamA',
-#             'Content-Type': 'application/x-www-form-urlencoded',
-#         }
-#         resp = requests.post(url, headers=headers, data=json_string)
-#         doi_pid = resp.text.replace('"', '').replace('{', '').replace('}', '')
+    #         headers = {
+    #             'Content-Type': 'application/json',
+    #             'Authorization': 'Bearer @km1-10122004-lamA',
+    #             'Content-Type': 'application/x-www-form-urlencoded',
+    #         }
+    #         resp = requests.post(url, headers=headers, data=json_string)
+    #         doi_pid = resp.text.replace('"', '').replace('{', '').replace('}', '')
 
     if DATAVERSE_ID == 'liss_dc' or DATAVERSE_ID == 'dans-easy':
-        pid_field = next(filter(lambda x: x['mapping'] == 'doi' and 'doi' in x['value'], semafcli.cmdigraph.exportrecords))
+        pid_field = next(
+            filter(lambda x: x['mapping'] == 'doi' and 'doi' in x['value'], semafcli.cmdigraph.exportrecords))
         doi_pid = 'doi:' + pid_field['value'].split("/", 3)[3]
 
     status = semafcli.dataset_upload(metadatafile, doi_pid)
